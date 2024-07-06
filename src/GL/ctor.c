265115f1a3400e24df2dd06db46929fbe1b3d9a3
SonoSooS <robonyunyu@gmail.com>
2024-07-06 06:45:39 +0200

    Reduce code size

--- src/GL/ctor.c
+++ src/GL/ctor.c
@@ -11,21 +11,24 @@ DWORD (WINAPI*mGetModuleBaseNameA)
     DWORD   nSize
 );
 
-/*void __attribute__((noreturn)) WINAPI err()
+/*void __attribute__((noreturn)) WINAPI err() // not (void) !
 {
     void* retaddr = __builtin_return_address(0);
     printf("Call nullptr to %016llX\n", retaddr);
     fflush(stdout);
-    *((size_t*)0) = 0xC0C0CACE;
-    //return;
+    __builtin_unreachable();
 }*/
 
 void* __attribute__((noinline)) GL_GetProcAddress(const char* name)
 {
     size_t ptr = (size_t)wglGetProcAddress(name);
     
+    // why can't some drivers decide what to return as invalid value???
     if(!(ptr >> 12) || (ptr >> 12) == ((~0) >> 12))
+    {
+        // try fish out the missing function from the stock DLL (already a lost cause though)
         ptr = (size_t)GetProcAddress(GetModuleHandleA("opengl32"), name);
+    }
     
     if(!ptr)
     {
@@ -55,22 +58,31 @@ void* __attribute__((noinline)) GL_GetProcAddress(const char* name)
     return (void*)ptr;*/
 }
 
-extern void(WINAPI*glDummyFunc)(void);
-extern void(WINAPI*glDummyFuncEnd)(void);
+extern void(WINAPI*glAAADummyFunc)(void);
+extern void(WINAPI*glZZZDummyFuncEnd)(void);
 
 int __attribute__((optimize("Os"))) GL_LinkFunctions()
 {
     int cnt = 0;
     
-#define GLDEFINE(proc, fn) { fn = (proc)GL_GetProcAddress(#fn); }
     
-    glDummyFunc = 0;
-    
-#include "core.h"
-    
-    glDummyFuncEnd = 0;
-    
+    glAAADummyFunc = 0;
+//#define GLDEFINE(proc, fn) { fn = (proc)GL_GetProcAddress(#fn); }
+//#include "core.h"
+    glZZZDummyFuncEnd = 0;
 #undef GLDEFINE
+    
+    void** ptr = (void*)(((void**)&glAAADummyFunc) + 1);
+    void** ptr_end = (void*)(((void**)&glZZZDummyFuncEnd) - 0);
+    
+    while(ptr < ptr_end)
+    {
+        const char* name = *ptr;
+        if(name)
+            *ptr = (void*)GL_GetProcAddress(name);
+        
+        ptr++;
+    }
     
     if(glGetString)
     {
@@ -81,10 +93,11 @@ int __attribute__((optimize("Os"))) GL_LinkFunctions()
     return cnt;
 }
 
-void(WINAPI*glDummyFunc)(void);
 
-#define GLDEFINE(proc, name) proc name ;
+void(WINAPI*glAAADummyFunc)(void) = 0;
+
+#define GLDEFINE(proc, name) proc name = (const void*)(const char* const) #name ;
 #include "core.h"
 #undef GLDEFINE
 
-void(WINAPI*glDummyFuncEnd)(void);
+void(WINAPI*glZZZDummyFuncEnd)(void) = 0;
